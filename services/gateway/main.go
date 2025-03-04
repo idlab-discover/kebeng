38a1f80550a10fc62dfa5113cd0f02f48e07ebb0
Bram Van Den Eeckhout <bravdnee.vandeneeckhout@ugent.be>
2025-03-04 17:10:49 +0100

    added 'private' (boolean) field to snap_entries

--- services/gateway/main.go
+++ services/gateway/main.go
@@ -1,49 +1,45 @@
 package main
 
 import (
+	"github.com/idlab-discover/kebeng/services/gateway/internal/config"
+	"github.com/sirupsen/logrus"
 
-    "github.com/idlab-discover/kebeng/services/gateway/internal/config"
-    "github.com/sirupsen/logrus"
-
-    accClient "github.com/idlab-discover/kebeng/services/account/client"
-    storeClient "github.com/idlab-discover/kebeng/services/store/client"
-    "github.com/idlab-discover/kebeng/services/gateway/handler"
-    "github.com/idlab-discover/kebeng/services/gateway/internal/middleware"
 	"github.com/gin-gonic/gin"
-
+	accClient "github.com/idlab-discover/kebeng/services/account/client"
+	"github.com/idlab-discover/kebeng/services/gateway/handler"
+	"github.com/idlab-discover/kebeng/services/gateway/internal/middleware"
+	storeClient "github.com/idlab-discover/kebeng/services/store/client"
 )
 
 func main() {
-    cfg, err := config.LoadConfig()
-    if err != nil {
-        logrus.Fatalf("Error loading config: %v", err)
-    }
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		logrus.Fatalf("Error loading config: %v", err)
+	}
 
-    logrus.Infof("Loaded config: %+v", cfg)
+	logrus.Infof("Loaded config: %+v", cfg)
 
-    
-    // TODO: if can't connect to service or create client TRY AGAIN 
+	// TODO: if can't connect to service or create client TRY AGAIN
 
-    //TODO: check how bigger load works and multiple requests
-    // need more connections? or how does grpc handle this
-    accountClient, err := accClient.NewAccountClient(cfg.AccountServiceHost, cfg.AccountServicePort)
-    if err != nil {
-        logrus.Errorf("could not create account client: %v", err)
-    }
-        
- 
-    storeClient, err := storeClient.NewStoreClient()
-    if err != nil {
-        logrus.Errorf("could not create store client: %v", err)
-    }
-    
-    handler := handler.NewHandler(accountClient, storeClient, cfg)
-    
+	//TODO: check how bigger load works and multiple requests
+	// need more connections? or how does grpc handle this
+	accountClient, err := accClient.NewAccountClient(cfg.AccountServiceHost, cfg.AccountServicePort)
+	if err != nil {
+		logrus.Errorf("could not create account client: %v", err)
+	}
+
+	storeClient, err := storeClient.NewStoreClient(cfg.StoreServiceHost, cfg.StoreServicePort)
+	if err != nil {
+		logrus.Errorf("could not create store client: %v", err)
+	}
+
+	handler := handler.NewHandler(accountClient, storeClient, cfg)
+
 	// Setup gin and routes
 	r := gin.Default()
 	if cfg.DebugMode {
 		logrus.Info("Debug mode enabled")
-        logrus.SetLevel(logrus.DebugLevel)
+		logrus.SetLevel(logrus.DebugLevel)
 		r.Use(middleware.RequestLoggerMiddleware())
 	} else {
 		logrus.Info("Debug mode disabled")
@@ -53,12 +49,10 @@ func main() {
 		c.JSON(404, gin.H{"code": "KEBENG STORE: PAGE_NOT_FOUND", "message": "Page not found"})
 	})
 
-    handler.SetupEndpoints(r)
+	handler.SetupEndpoints(r)
 
-    err = r.Run()
-    if err != nil {
-        logrus.Fatalf("Error running server: %v", err)
-    }
+	err = r.Run()
+	if err != nil {
+		logrus.Fatalf("Error running server: %v", err)
+	}
 }
-
-
