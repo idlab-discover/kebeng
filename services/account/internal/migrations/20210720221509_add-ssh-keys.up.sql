f92ef4caedee1484c3615b85b5a4ba147ac8f7f2
Diesel300300 <joran.verbruggen@ugent.be>
2025-03-17 13:16:48 +0100

    update rest of repo functions to sqlx variants

--- services/account/internal/migrations/20210720221509_add-ssh-keys.up.sql
+++ services/account/internal/migrations/20210720221509_add-ssh-keys.up.sql
@@ -5,8 +5,8 @@ create sequence public.ssh_keys_id_seq;
 CREATE TABLE IF NOT EXISTS public.ssh_keys
 (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
-    created_at timestamp with time zone,
-    updated_at timestamp with time zone,
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone DEFAULT now(),
     deleted_at timestamp with time zone,
     public_key_string text COLLATE pg_catalog."default",
     account_id uuid,
