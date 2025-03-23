4f8ad42f97e04192d6887ee4f5e2dff4c8c6cdbf
Diesel300300 <joran.verbruggen@ugent.be>
2025-03-23 12:03:34 +0100

    add mock logic functions for account service

---
+++ services/account/internal/logic/mock_logic.go
@@ -0,0 +1,124 @@
+package logic
+
+import (
+	"context"
+
+	proto "github.com/idlab-discover/kebeng/services/account/proto"
+	"github.com/stretchr/testify/mock"
+	"google.golang.org/grpc"
+)
+
+// MockAccountServiceClient is a mock implementation of the AccountServiceClient interface.
+type MockAccountServiceClient struct {
+	mock.Mock
+}
+
+// PatchAccountByEmail is implemented as follows.
+func (m *MockAccountServiceClient) PatchAccountByEmail(ctx context.Context, in *proto.PatchAccountByEmailRequest, opts ...grpc.CallOption) (*proto.PatchAccountByEmailResponse, error) {
+	args := m.Called(ctx, in)
+	resp := args.Get(0)
+	if resp == nil {
+		return nil, args.Error(1)
+	}
+	return resp.(*proto.PatchAccountByEmailResponse), args.Error(1)
+}
+
+// CreateAccount mocks the CreateAccount method.
+func (m *MockAccountServiceClient) CreateAccount(ctx context.Context, in *proto.CreateAccountRequest, opts ...grpc.CallOption) (*proto.AccountResponse, error) {
+	args := m.Called(ctx, in)
+	resp := args.Get(0)
+	if resp == nil {
+		return nil, args.Error(1)
+	}
+	return resp.(*proto.AccountResponse), args.Error(1)
+}
+
+// UpdateAccount mocks the UpdateAccount method.
+func (m *MockAccountServiceClient) UpdateAccount(ctx context.Context, in *proto.UpdateAccountRequest, opts ...grpc.CallOption) (*proto.AccountResponse, error) {
+	args := m.Called(ctx, in)
+	resp := args.Get(0)
+	if resp == nil {
+		return nil, args.Error(1)
+	}
+	return resp.(*proto.AccountResponse), args.Error(1)
+}
+
+// DeleteAccount mocks the DeleteAccount method.
+func (m *MockAccountServiceClient) DeleteAccount(ctx context.Context, in *proto.DeleteAccountRequest, opts ...grpc.CallOption) (*proto.DeleteAccountResponse, error) {
+	args := m.Called(ctx, in)
+	resp := args.Get(0)
+	if resp == nil {
+		return nil, args.Error(1)
+	}
+	return resp.(*proto.DeleteAccountResponse), args.Error(1)
+}
+
+// GetAccountByEmail mocks the GetAccountByEmail method.
+func (m *MockAccountServiceClient) GetAccountByEmail(ctx context.Context, in *proto.GetAccountByEmailRequest, opts ...grpc.CallOption) (*proto.AccountResponse, error) {
+	args := m.Called(ctx, in)
+	resp := args.Get(0)
+	if resp == nil {
+		return nil, args.Error(1)
+	}
+	return resp.(*proto.AccountResponse), args.Error(1)
+}
+
+// GetAccountByID mocks the GetAccountByID method.
+func (m *MockAccountServiceClient) GetAccountByID(ctx context.Context, in *proto.GetAccountByIDRequest, opts ...grpc.CallOption) (*proto.AccountResponse, error) {
+	args := m.Called(ctx, in)
+	resp := args.Get(0)
+	if resp == nil {
+		return nil, args.Error(1)
+	}
+	return resp.(*proto.AccountResponse), args.Error(1)
+}
+
+// GetAccountsByIds mocks the GetAccountsByIds method.
+func (m *MockAccountServiceClient) GetAccountsByIds(ctx context.Context, in *proto.GetAccountsByIdsRequest, opts ...grpc.CallOption) (*proto.GetAccountsByIdsResponse, error) {
+	args := m.Called(ctx, in)
+	resp := args.Get(0)
+	if resp == nil {
+		return nil, args.Error(1)
+	}
+	return resp.(*proto.GetAccountsByIdsResponse), args.Error(1)
+}
+
+// GetAccountByUsername mocks the GetAccountByUsername method.
+func (m *MockAccountServiceClient) GetAccountByUsername(ctx context.Context, in *proto.GetAccountByUsernameRequest, opts ...grpc.CallOption) (*proto.AccountResponse, error) {
+	args := m.Called(ctx, in)
+	resp := args.Get(0)
+	if resp == nil {
+		return nil, args.Error(1)
+	}
+	return resp.(*proto.AccountResponse), args.Error(1)
+}
+
+// AddKey mocks the AddKey method.
+func (m *MockAccountServiceClient) AddKey(ctx context.Context, in *proto.AddKeyRequest, opts ...grpc.CallOption) (*proto.KeyResponse, error) {
+	args := m.Called(ctx, in)
+	resp := args.Get(0)
+	if resp == nil {
+		return nil, args.Error(1)
+	}
+	return resp.(*proto.KeyResponse), args.Error(1)
+}
+
+// GetKeyBySHA3384 mocks the GetKeyBySHA3384 method.
+func (m *MockAccountServiceClient) GetKeyBySHA3384(ctx context.Context, in *proto.GetKeyBySHA3384Request, opts ...grpc.CallOption) (*proto.KeyResponse, error) {
+	args := m.Called(ctx, in)
+	resp := args.Get(0)
+	if resp == nil {
+		return nil, args.Error(1)
+	}
+	return resp.(*proto.KeyResponse), args.Error(1)
+}
+
+// GetKeysByAccountId mocks the GetKeysByAccountId method.
+func (m *MockAccountServiceClient) GetKeysByAccountId(ctx context.Context, in *proto.GetKeysByAccountIdRequest, opts ...grpc.CallOption) (*proto.KeysResponse, error) {
+	args := m.Called(ctx, in)
+	resp := args.Get(0)
+	if resp == nil {
+		return nil, args.Error(1)
+	}
+	return resp.(*proto.KeysResponse), args.Error(1)
+}
