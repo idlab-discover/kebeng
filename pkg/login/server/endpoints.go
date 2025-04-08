36513c2a80dcbbfc9e8b96c55cb212fde5594c6e
Diesel300300 <joran.verbruggen@ugent.be>
2025-04-08 10:40:34 +0200

    remove all not used files

--- pkg/login/server/endpoints.go
+++
@@ -1,8 +0,0 @@
-package server
-
-import "github.com/gin-gonic/gin"
-
-func (s *Server) SetupEndpoints(r *gin.Engine) {
-	r.POST("/api/v2/tokens/discharge", s.dischargeTokens)
-	r.GET("/api/v2/keys/:email", s.getSSHKeys)
-}
--- scripts/postgres_backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-HOST=$1
-POST=$2
-pg_dump -h ${HOST}  -p ${PORT} -U manager --column-inserts --data-only  store > /backup/store_inserts.sql
