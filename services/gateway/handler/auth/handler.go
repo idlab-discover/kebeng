c543151feb58d68b22c637f77b6d9afb71545be0
Diesel300300 <joran.verbruggen@ugent.be>
2025-04-08 20:30:00 +0200

    write test for GetMacaroonsFromString

--- services/gateway/handler/auth/handler.go
+++ services/gateway/handler/auth/handler.go
@@ -15,54 +15,9 @@ type Handler struct {
 }
 
 // TODO: implement correctly to many unknows of what has to be included and what not, need good source
+// or develop own authentication service, instead of using Ubuntu One SSO
 func (h *Handler) VerifyMacaroon(c *gin.Context) {
-	// not implemented for now
 	c.JSON(http.StatusNotImplemented, gin.H{"error_list": cerror.NewError(cerror.NotImplemented, "not implemented too many unknowns of implementation")})
-	/*
-			var req *message.VerifyRequest
-			el := cerror.NewErrorList()
-			if err := c.ShouldBindJSON(&req); err != nil {
-				el.Add(cerror.BadRequest, cerror.FormatBindError(err))
-				c.JSON(el.GetHTTPStatus(),gin.H{"error_list": el,})
-				return
-			}
-
-		    // TODO: Don't know if you can get email from this actually or not (from discharge macaroon normally?)
-			userEmail := auth.VerifyAndGetEmail(h.config, el, req.AuthData.Authorization)
-		    // need userEmail to continue
-			if userEmail == nil {
-		        c.JSON(el.GetHTTPStatus(), gin.H{"error_list": el})
-		        return
-			}
-
-		    // TODO: change account client to use errorList
-			user, _ := h.AccountClient.GetAccountByEmail(*userEmail)
-		    if user == nil {
-		        el.Add(cerror.ResourceNotFound, fmt.Sprintf("user with email %s not found", *userEmail))
-		        c.JSON(el.GetHTTPStatus(), gin.H{"error_list": el})
-		        return
-		    }
-
-		    // TODO: check wtf these values have to be
-		    // probably get them from the macaroon
-		    _ = message.VerifyResponse{
-		        Allowed:               true, // idk what dertemines this
-		        DeviceRefreshRequired: false, // obsolote
-		        RefreshRequired:       false, // check with expiry time of macaroon
-		        Account: &message.VerifyAccount{
-		            Email:       user.Email,
-		            DisplayName: user.DisplayName,
-		            OpenId:      "oid1234", // what id is this?
-		            Verified:    true, // idk what this is
-		        },
-		        Device:      nil, // obsolete
-		        LastAuth:    "2016-05-26T12:53:23Z", // should be from macaroon
-		        Permissions: &[]string{"package_access", "package_manage", "package_push", "package_register", "package_release", "package_update"}, // should be from macaroon
-		        SnapIds:     nil, // should be from macaroon
-		        Channels:    nil, // should be from macaroon
-		    }
-		    return
-	*/
 }
 
 func (h *Handler) GenerateMacaroon(c *gin.Context) {
