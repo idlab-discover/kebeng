7dcc733dcd4135156f7af595dc61a0d76cadb361
Diesel300300 <joran.verbruggen@ugent.be>
2025-04-11 18:15:55 +0200

    wrote mock client functions

--- services/assertion/client/mock_client.go
+++ services/assertion/client/mock_client.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"time"
+
 	proto "github.com/idlab-discover/kebeng/services/assertion/proto"
 	"github.com/stretchr/testify/mock"
 )
@@ -21,6 +23,22 @@ func (m *MockAssertionClient) Close() {
 func (m *MockAssertionClient) ProcessSnapBuildAssertion(assertion []byte) *proto.SnapBuildAssertionResponse {
 	args := m.Called(assertion)
 	if resp, ok := args.Get(0).(*proto.SnapBuildAssertionResponse); ok {
+		return resp
+	}
+	return nil
+}
+
+func (m *MockAssertionClient) AddAccountKeyAssertion(revisionSequenceNumber uint32, publicKeySha3_384 string, accountId string, name string, since *time.Time, until *time.Time, body []byte) *proto.AccountKeyAssertionResponse {
+	args := m.Called(revisionSequenceNumber, publicKeySha3_384, accountId, name, since, until, body)
+	if resp, ok := args.Get(0).(*proto.AccountKeyAssertionResponse); ok {
+		return resp
+	}
+	return nil
+}
+
+func (m *MockAssertionClient) AddSnapRevisionAssertion(snapSha3_384 string, developerId string, snapEntryId string, snapRevisionSequenceNumber uint32, snapSize uint64, timestamp *time.Time) *proto.SnapRevisionAssertionResponse {
+	args := m.Called(snapSha3_384, developerId, snapEntryId, snapRevisionSequenceNumber, snapSize, timestamp)
+	if resp, ok := args.Get(0).(*proto.SnapRevisionAssertionResponse); ok {
 		return resp
 	}
 	return nil
