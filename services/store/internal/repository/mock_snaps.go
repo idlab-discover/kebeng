ba55268259e44165deb74f4a9b027f89dbcc3ea3
Bram Van Den Eeckhout <bravdnee.vandeneeckhout@ugent.be>
2025-04-12 19:59:39 +0200

    added ErrorList to AddRevision

--- services/store/internal/repository/mock_snaps.go
+++ services/store/internal/repository/mock_snaps.go
@@ -31,7 +31,7 @@ func (m *MockSnapsRepository) AddDefaultChannels(snapEntryId uuid.UUID, snapTrac
 	return nil
 }
 
-func (m *MockSnapsRepository) AddRevision(entryId uuid.UUID, trackId uuid.UUID, channelId uuid.UUID, snapName string, size uint64, sequenceNumber uint, architectures []string, sha3_384 string) (*models.SnapRevision, *cerror.CustomError) {
+func (m *MockSnapsRepository) AddRevision(entryId uuid.UUID, trackId uuid.UUID, channelId uuid.UUID, snapName string, size uint64, sequenceNumber uint, architectures []string, sha3_384 string, el *cerror.ErrorList) (*models.SnapRevision, *cerror.CustomError) {
 	args := m.Called(entryId, trackId, channelId, snapName, size, sequenceNumber)
 	if args.Get(0) != nil {
 		return args.Get(0).(*models.SnapRevision), nil
