007d1c500143cc3aaf5cda112da6ea367c837f78
Diesel300300 <joran.verbruggen@ugent.be>
2025-04-12 16:34:51 +0200

    update client interface

--- services/assertion/client/client.go
+++ services/assertion/client/client.go
@@ -18,6 +18,8 @@ import (
 
 type AssertionClientInterface interface {
 	ProcessSnapBuildAssertion(assertion []byte) *proto.SnapBuildAssertionResponse
+	AddAccountKeyAssertion(revisionSequenceNumber uint32, publicKeySha3_384 string, accountId string, name string, since *time.Time, until *time.Time, body []byte) *proto.AccountKeyAssertionResponse
+	AddSnapRevisionAssertion(snapSha3_384 string, developerId string, snapEntryId string, snapRevisionSequenceNumber uint32, snapSize uint64, timestamp *time.Time) *proto.SnapRevisionAssertionResponse
 	Close()
 }
 
