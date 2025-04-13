25365274430be17eb1d7a5ea90fed1cacca1fa49
Diesel300300 <joran.verbruggen@ugent.be>
2025-04-13 18:28:55 +0200

    remove load_test_data from account service

--- services/account/main.go
+++ services/account/main.go
@@ -33,10 +33,7 @@ func main() {
 
 	// if in test mode, load test data
 	if cfg.TestMode {
-		err := database.LoadTestData(cfg.TestDataFilePath, db, repo)
-		if err != nil {
-			logrus.Fatalf("Failed to load test data: %v", err)
-		}
+		logrus.Infof("Test mode enabled")
 	}
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", cfg.GRPCHost, cfg.GRPCPort))
