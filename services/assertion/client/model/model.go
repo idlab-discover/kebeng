8743b404df1589a205ec7f34edaff32e5557d18d
Diesel300300 <joran.verbruggen@ugent.be>
2025-04-19 21:45:01 +0200

    wrote AddSnapDeclarationAssertion logic function

--- services/assertion/client/model/model.go
+++ services/assertion/client/model/model.go
@@ -8,6 +8,7 @@ type Alias struct {
 // TODO: both Plug and Slot should be able to have more complex types than just bools (see https://ubuntu.com/core/docs/reference/assertions/snap-declaration)
 // aswel but keep simple for now
 
+// NOTE: maybe change this to use an enum? with states like Allowed, Denied and NotSet and can be expanded later on to support more complex objects
 // make pointers out of the bool values so that we can use nil to indicate that the field is not set
 type Plug struct {
 	AllowInstallation   *bool
