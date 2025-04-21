9349dd70b896b74d5727a1071b631793b0242463
Diesel300300 <joran.verbruggen@ugent.be>
2025-04-21 15:50:37 +0200

    add default validation

--- services/account/internal/logic/logic.go
+++ services/account/internal/logic/logic.go
@@ -40,11 +40,15 @@ func ptrTimeToPtrTimestamp(t *time.Time) *timestamppb.Timestamp {
 
 func (a *AccountService) AddAccount(ctx context.Context, req *proto.AddAccountRequest) (*proto.AccountResponse, error) {
 	el := make([]*cerrorpb.Error, 0)
+	// TODO: figure out where validation field has to be set for now set to valid <unproven | certified >
+	// set default to unproven
+	validation := "unproven"
 	account := &models.Account{
 		DisplayName:  req.DisplayName,
 		Username:     req.Username,
 		Email:        req.Email,
 		PasswordHash: req.HashedPassword,
+		Validation:   &validation,
 	}
 
 	createdAccount, err := a.repo.AddAccount(ctx, account)
