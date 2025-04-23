781b34805a3c0cb2e7224f961c44bc86d1d9c15c
Bram Van Den Eeckhout <bravdnee.vandeneeckhout@ugent.be>
2025-04-23 18:04:32 +0200

    updated snapEntry with metadata

--- services/gateway/handler/snap/handler.go
+++ services/gateway/handler/snap/handler.go
@@ -343,17 +343,24 @@ func (h *Handler) SnapPush(c *gin.Context) {
 	})
 
 	// After the status details URL is returned, we can proceed with creating a new revision
-	// We need the sha3_384_encoded hash of the snap package
+	// We need the sha3_384_encoded hash of the snap package, and other metadata
 	metadata := h.StoreClient.GetObjectCustomMetadata("unscanned", req.UnscannedFileName)
 	if len(metadata.Errors) > 0 {
 		el.ExtendProtoError(metadata.Errors)
 		return
 	}
-
 	logrus.Debugf("Metadata: %v", metadata)
 
+	// Update the snapEntry with the metadata
+	updatedEntry := h.StoreClient.UpdateSnapEntryWithMetadata(parsedEntryUUID, metadata)
+	if len(updatedEntry.Errors) > 0 {
+		el.ExtendProtoError(updatedEntry.Errors)
+		return
+	}
+	logrus.Debugf("Updated entry: %v", updatedEntry)
+
 	// Create a new revision for the snap upload
-	revision := h.StoreClient.AddRevision(entry.SnapName, metadata.GetSha3_384Encoded(), uint64(req.BinaryFileSize), []string{"amd64"}, req.Channels, req.UnscannedFileName) // FIX: architectures should be passed from the request
+	revision := h.StoreClient.AddRevision(entry.SnapName, metadata.GetSha3_384Encoded(), uint64(req.BinaryFileSize), metadata.Architectures, req.Channels, req.UnscannedFileName) // FIX: architectures should be passed from the request
 	if len(revision.Errors) > 0 {
 		el.ExtendProtoError(revision.Errors)
 	}
