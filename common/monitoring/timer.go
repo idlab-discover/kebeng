3d410397ad0fe3f4a31962c79bf89e8f38814003
Diesel300300 <joran.verbruggen@ugent.be>
2025-04-24 22:46:40 +0200

    update timer buckets

--- common/monitoring/timer.go
+++ common/monitoring/timer.go
@@ -9,9 +9,14 @@ import (
 var (
 	requestDuration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "request_duration_seconds",
-			Help:    "Duration of HTTP requests in seconds",
-			Buckets: []float64{.025, .005, .0075, .01, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1},
+			Name: "request_duration_seconds",
+			Help: "Duration of HTTP requests in seconds",
+			Buckets: []float64{
+				0.05, 0.1, 0.15, 0.2, 0.25, 0.3,
+				0.35, 0.4, 0.45, 0.5, 0.55,
+				0.6, 0.65, 0.7, 0.75, 0.8,
+				0.85, 0.9, 0.95, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5,
+			},
 		},
 		[]string{"handlerFunction"},
 	)
