27d0a068f2f7cd375e0497bf856d4d2117b82292
Diesel300300 <joran.verbruggen@icloud.com>
2025-05-13 16:05:55 +0200

    update bucket size

--- common/monitoring/metrics.go
+++ common/monitoring/metrics.go
@@ -51,9 +51,11 @@ var (
 
 	monitoringRequestDuration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "request_duration_seconds_monitoring",
-			Help:    "Duration of HTTP requests measured in monitoring service in seconds",
-			Buckets: generateBuckets(120, 0, 4),
+			Name:                            "request_duration_seconds_monitoring",
+			Help:                            "Duration of HTTP requests measured in monitoring service in seconds",
+			NativeHistogramBucketFactor:     1.1,
+			NativeHistogramMaxBucketNumber:  200,
+			NativeHistogramMinResetDuration: 1 * time.Hour,
 		},
 		[]string{"handlerFunction"},
 	)
