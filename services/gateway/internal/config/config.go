a88fe10dfde79fbf9fd38095053c59a848849096
Caspar Nuël <casparnuel@yandex.com>
2026-04-30 00:08:22 +0200

    fix(gateway): make config verification error reflect key name correctly

--- services/gateway/internal/config/config.go
+++ services/gateway/internal/config/config.go
@@ -140,7 +140,7 @@ func (c *Config) checkConfig() error {
 	}
 
 	if c.CohortSigningKey == "" {
-		errs = append(errs, "cohort siging key is required")
+		errs = append(errs, "cohort_siging_key is required")
 	}
 
 	// Test mode settings
