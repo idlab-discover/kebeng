80b1fa90549bf1a897719159f2654be1b48231c0
Caspar Nuël <casparnuel@yandex.com>
2026-05-02 00:02:49 +0200

    wip(store): prepare for supporting multiple delta types

--- services/store/internal/migrations/010_create_snap_deltas.down.sql
+++ services/store/internal/migrations/010_create_snap_deltas.down.sql
@@ -1,3 +1,4 @@
 DROP TABLE IF EXISTS public.snap_deltas;
 
+DROP INDEX IF EXISTS idx_snap_deltas_source;
 DROP INDEX IF EXISTS idx_snap_deltas_target;
--- services/store/internal/migrations/010_create_snap_deltas.up.sql
+++ services/store/internal/migrations/010_create_snap_deltas.up.sql
@@ -6,10 +6,12 @@ CREATE TABLE IF NOT EXISTS public.snap_deltas
 	created_at timestamp with time zone DEFAULT now(),
 	source_revision_id uuid NOT NULL REFERENCES revision(id),
 	target_revision_id uuid NOT NULL REFERENCES revision(id),
-	minio_file_path TEXT COLLATE pg_catalog."default",
+	minio_file_path TEXT NOT NULL COLLATE pg_catalog."default",
+	format TEXT NOT NULL;
 	size bigint,
 	sha3_384_encoded text COLLATE pg_catalog."default",
-	UNIQUE (source_revision_id, target_revision_id)
+	UNIQUE (source_revision_id, target_revision_id, format)
 );
 
+CREATE INDEX idx_snap_deltas_source ON public.snap_deltas(source_revision_id);
 CREATE INDEX idx_snap_deltas_target ON public.snap_deltas(target_revision_id);
