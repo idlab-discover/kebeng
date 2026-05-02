1e6d5bdf0549866fa9e76822b3f29fc5aace2371
Caspar Nuël <casparnuel@yandex.com>
2026-05-02 12:05:39 +0200

    feat(store): provide strategy pattern for the generation of detlas

---
+++ services/store/internal/delta/delta.go
@@ -0,0 +1,74 @@
+package delta
+
+// TODO: Finish this implementation
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// Strategy pattern for different delta generation techniques
+// Every delta calculation tool needs random access to the source file
+// And _may_ need random access to the target file
+// *os.File for input guarantees random access, io.Reader for target allows simple scanning
+// or if needed a temporary disk file can also be created based on it for random access
+
+type DeltaGenerator interface {
+	Format() string
+	Generate(source *os.File, target io.Reader, out io.Writer) (uint64, error)
+}
+
+type Xdelta3Generator struct {
+	ctx context.Context
+}
+
+func NewXdelta3Generator(ctx context.Context) *Xdelta3Generator {
+	return &Xdelta3Generator{ctx}
+}
+
+func (g *Xdelta3Generator) Format() string {
+	return "xdelta3"
+}
+
+func (g *Xdelta3Generator) Generate(source *os.File, target io.Reader, out io.Writer) (uint64, error) {
+	cmd := exec.CommandContext(
+		g.ctx,
+		"xdelta3", "-e", "-f", "-S", "none",
+		"-s", source.Name(),
+		"-",
+	)
+
+	cmd.Stdin = target
+	
+	var written uint64
+	cmd.Stdout = writerCounter{w: out, n: &written}
+
+	var stderr strings.Builder
+	cmd.Stderr = &stderr
+
+	if err := cmd.Run(); err != nil {
+		return 0, fmt.Errorf("xdelta3 failed: %v (stderr: %s)", err, stderr.String())
+	}
+
+	return written, nil
+}
+
+// TODO: Add `snap-1-1-xdelta3` generator
+// (see https://github.com/canonical/snapd/blob/dee46fb8fe1b0d4f1f7c6d82270bdd43996011a7/snap/squashfs/delta.go#L43)
+// For "snap-aware" delta support
+
+// ===== UTILITIES =======
+type writerCounter struct {
+    w io.Writer
+    n *uint64
+}
+
+func (wc writerCounter) Write(p []byte) (int, error) {
+    nn, err := wc.w.Write(p)
+    *wc.n += uint64(nn)
+    return nn, err
+}
